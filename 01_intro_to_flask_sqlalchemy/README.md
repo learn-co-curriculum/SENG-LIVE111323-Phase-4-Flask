20a27f672bd998379885d1e9643016b9a8a2d88c
labradorescence <orangenewyorkcreativeagency@gmail.com>
2024-01-29 17:27:42 -0500

    02

--- 01_intro_to_flask_sqlalchemy/README.md
+++
@@ -1,7 +0,0 @@
-## 001 Intro to Flask SQLAlchemy
-* Request-Response Cycle
-* Web Servers and WSGI/Werkzeug
-* Creating a Flask Application
-* Flask Routing and Views
-* SQLAlechmy in Flask
-
--- 02_crud_flask_shell/seed.py
+++ 02_crud_flask_shell/seed.py
@@ -1,8 +1,15 @@
 #contains seed file, create instances 
+from random import choice as rc
+from faker import Faker
+
 from app import app 
 from models import Coffee, db 
 
 with app.app_context(): # create app context with app.app_context()
+    fake = Faker()
+    
+    coffee_list = []
+
     Coffee.query.delete()
 
     #create some seeds for coffee and commit them to the db
@@ -21,7 +28,17 @@ with app.app_context(): # create app context with app.app_context()
         description="the concentrated coffee, very strong!"
     )
 
+    for n in range(25):
+        cup = Coffee(
+            name=rc([ "Double Espresso", "Red Eye", "Black Eye", "Americano", "Long Black", "Macchiato", "Long Macchiato", "Cortado", "Breve", "Flat White", "Cafe Latte", "Mocha", "Vienna", "Affogato", "Cafe au Lait", "Iced Coffee"]),
+            image = fake.image_url(),
+            price=rc([3.50, 4.00, 4.50, 5.00, 5.50, 7.00]),
+            description = fake.catch_phrase()
+        )
+        coffee_list.append(cup)
+
     db.session.add_all([cappuccino, espresso])
+    db.session.add_all(coffee_list)
     db.session.commit() #commit! 
 
     print(" DONE SEEDING!!👌🎍😍")
