f7da3236221fa920c8a09cc4f597e4d1f2974bcd
labradorescence <orangenewyorkcreativeagency@gmail.com>
2024-03-05 12:09:16 -0500

    deploy

--- 91_deployment/client/src/components/Home.jsx
+++ 91_deployment/client/src/components/Home.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
-
+import CoffeeList from './CoffeeList'
 export default function Home() {
   return (
-    <div>Home</div>
+    <>
+    <CoffeeList />
+    </>
   )
 }
---
+++ 91_deployment/server/migrations/versions/a4b3a4e235ad_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: a4b3a4e235ad
+Revises: 9018f1dd3c9f
+Create Date: 2024-03-05 11:25:38.708442
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'a4b3a4e235ad'
+down_revision = '9018f1dd3c9f'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
